c68140b3cfbf9b6b9b8e76225f902c3000bd1c4a
MeshachArinze <meshacharinze@gmail.com>
2023-03-11 17:50:38 +0100

    commit to git repo

--- youtube_video/app.js
+++ youtube_video/app.js
@@ -2,4 +2,13 @@ const playPauseBtn = document.querySelector('play-pause-btn');
 const theaterBtn = document.querySelector('.theater-btn');
 const fullScreenBtn = document.querySelector('.full-screen-btn');
 const miniPlayerBtn = document.querySelector('.mini-player-btn');
-
+const muteBtn = document.querySelector(".mute-btn");
+const captionsBtn = document.querySelector(".speed-btn");
+const currentTimeElem = document.querySelector(".current-time");
+const totalTimeElem = document.querySelector(".total-time");
+const previewImg = document.querySelector(".previewImg");
+const thumbnailImg = document.querySelector(".thumbnailImg");
+const volumeSlider = document.querySelector(".volume-slider");
+const videoContainer = document.querySelector(".video-container");
+const timelineContainer = document.querySelector(".timeline-container");
+const video = document.querySelector("video");

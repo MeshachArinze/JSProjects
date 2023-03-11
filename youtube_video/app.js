1c6524b32c54dd27597232dab46a88a906f74a55
MeshachArinze <meshacharinze@gmail.com>
2023-03-11 18:27:06 +0100

    commit to initial repo

--- youtube_video/app.js
+++ youtube_video/app.js
@@ -1,7 +1,7 @@
-const playPauseBtn = document.querySelector('play-pause-btn');
-const theaterBtn = document.querySelector('.theater-btn');
-const fullScreenBtn = document.querySelector('.full-screen-btn');
-const miniPlayerBtn = document.querySelector('.mini-player-btn');
+const playPauseBtn = document.querySelector("play-pause-btn");
+const theaterBtn = document.querySelector(".theater-btn");
+const fullScreenBtn = document.querySelector(".full-screen-btn");
+const miniPlayerBtn = document.querySelector(".mini-player-btn");
 const muteBtn = document.querySelector(".mute-btn");
 const captionsBtn = document.querySelector(".speed-btn");
 const currentTimeElem = document.querySelector(".current-time");
@@ -12,3 +12,71 @@ const volumeSlider = document.querySelector(".volume-slider");
 const videoContainer = document.querySelector(".video-container");
 const timelineContainer = document.querySelector(".timeline-container");
 const video = document.querySelector("video");
+
+document.addEventListener("keydown", (e) => {
+  const tagName = document.activeElement.tagName.toLowerCase();
+
+  if (tagName === "input") return;
+
+  switch (e.key.toLowerCase()) {
+    case " ":
+      if (tagName === " button") return;
+    case " k":
+      togglePlay();
+      break;
+    case "f":
+      toggleFullScreenMode();
+      break;
+    case "t":
+      toggleTheaterMode();
+      break;
+    case "i":
+      toggleMiniPlayerMode();
+      break;
+    case "m":
+      toggleMute();
+      break;
+    case "arrowleft":
+    case "j":
+      skip(-5);
+      break;
+    case "arrowright":
+    case "l":
+      skip(5);
+      break;
+    case "c":
+      toggleCaptions();
+      break;
+  }
+});
+
+// Timeline
+timelineContainer.addEventListener("mousemove", handleTimelineUpdate);
+timelineContainer.addEventListener("mousedown", toggleScrubbing);
+
+document.addEventListener("mouseup", (e) => {
+  if (isScrubbing) toggleSrubbing(e);
+});
+
+document.addEventListener("mousemove", (e) => {
+  if (isScrubbing) handleTimelineUpdate(e);
+});
+
+let isScrubbing = false;
+let wasPaused;
+
+function toggleScrubbing(e) {
+    const rect = timelineContainer.getBoundingClientRect();
+    const percent = Math.min(Math.max(0, e.x - rect.x), rect.width) / rect.width;
+    isScrubbing = (e.buttons & 1) === 1;
+    videoContainer.classList.toggle("scrubbing", isScrubbing)
+    if (isScrubbing) {
+        wasPaused = video.paused;
+        video.pause();
+    } else {
+        video.currentTime = percent * video.duration;
+        if (!wasPaused) video.play
+    }
+
+    handleTimelineUpdate(e);
+}

943d1e52226c9fec55d7d1828016a2f5414da207
MeshachArinze <meshacharinze@gmail.com>
2023-03-11 17:32:08 +0100

    commit to github

---
+++ youtube_video/app.js
@@ -0,0 +1,5 @@
+const playPauseBtn = document.querySelector('play-pause-btn');
+const theaterBtn = document.querySelector('.theater-btn');
+const fullScreenBtn = document.querySelector('.full-screen-btn');
+const miniPlayerBtn = document.querySelector('.mini-player-btn');
+
